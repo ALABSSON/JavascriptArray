87ae84d709f9ce4a0962d5d51344ddbea2dc6d90
Alabi Yusuf <yusufolatunjialabi@gmail.com>
2022-04-23 14:29:02 +0100

    add

---
+++ javascript/App.js
@@ -0,0 +1,30 @@
+
+// ARRAY OF ELEMENTS START
+    // let buy = ["Cake", "Apple", "Shoe"]
+    // console.log(buy)
+
+    // buy.push("cloth")
+    // console.log(buy) 
+// ARRAY OF ELEMENTS END
+
+
+// ARRAY OF OBJECTS START
+    let Info = [
+        {
+            Name : "Eflex",
+            Age: "12",
+            School: "Eflex Code"
+        },
+
+        {
+            Name : "Grace",
+            Age: "18",
+            School: "CodeLab"
+        }
+    ]
+
+    console.log(Info)
+    console.log(Info[0])
+    console.log(Info[0].Name)
+
+// ARRAY OF OBJECTS END
